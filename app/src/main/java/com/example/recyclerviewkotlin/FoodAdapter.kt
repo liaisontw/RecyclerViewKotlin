abf759a353d5ed8329ff60fecfae5cb2fa377b49
liaisontw <liaison.tw@gmail.com>
2023-11-06 10:29:28 +0800

    Init commit: pizza: RecyclerView With Item Click Listener in Kotlin : ( Android Tutorial 2022 )

---
+++ app/src/main/java/com/example/recyclerviewkotlin/FoodAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.recyclerviewkotlin
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class FoodAdapter(private val foodList: ArrayList<Food>)
+    : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>(){
+
+    class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView : ImageView = itemView.findViewById(R.id.imageView)
+        val textView : TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.each_item, parent, false)
+        return FoodViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return foodList.size
+    }
+
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        val food = foodList[position]
+        holder.imageView.setImageResource(food.image)
+        holder.textView.text = food.name
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.recyclerviewkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var foodList: ArrayList<Food>
+    private lateinit var foodAdapter: FoodAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        foodList = ArrayList()
+
+        foodList.add(Food(R.drawable.pizza_1, "pizza1"))
+        foodList.add(Food(R.drawable.pizza_2, "pizza2"))
+        foodList.add(Food(R.drawable.pizza_3, "pizza3"))
+        foodList.add(Food(R.drawable.pizza_4, "pizza4"))
+
+        foodAdapter = FoodAdapter(foodList)
+        recyclerView.adapter = foodAdapter
+    }
+}
